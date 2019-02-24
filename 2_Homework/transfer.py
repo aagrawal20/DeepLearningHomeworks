ca9a6684ff39630e8407c8fd64eee75ed6068678
Atharva Tendle <atharva.tendle@huskers.unl.edu>
2019-02-24 12:26:09 -0600

    Updated skeleton

--- 2_Homework/transfer.py
+++ 2_Homework/transfer.py
@@ -13,10 +13,23 @@ print(session.graph.get_operations())
 graph = session.graph
 # tensors
 x = graph.get_tensor_by_name('input_placeholder:0')
+
+# encode layers
+encode_1 = graph.get_tensor_by_name('encode_1/Relu:0')
+
+encode_2 = graph.get_tensor_by_name('encode_2/Relu:0')
+
+encode_3 = graph.get_tensor_by_name('encode_3/Relu:0')
+
+# flat layer
+flat = graph.get_tensor_by_name('flat')
+
 # code layer
-code_layer = graph.get_tensor_by_name('conv_net_2d_1/Relu:0')
+code_layer = graph.get_tensor_by_name('code/Relu:0')
+
 # code without gradients
 code_no_grad = tf.reshape(tf.stop_gradient(code_layer), [-1, height, width, channels])
+
 # dense 1
 dense_1 = tf.layers.dense(code_no_grad, 1024, name="dense1")
 # dense 2
