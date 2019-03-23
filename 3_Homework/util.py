959aa41fca861b4cb819a0a36f8c5be7c7fbd8db
Atharva Tendle <atharva.tendle@huskers.unl.edu>
2019-03-23 09:53:58 -0500

    Initial commit for homework 3

---
+++ 3_Homework/util.py
@@ -0,0 +1,51 @@
+import random
+import math
+import tensorflow as tf
+import collections
+
+Transition = collections.namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+def select_eps_greedy_action(policy_model, obs, step, num_actions, EPS_START=1, EPS_END=0.1, EPS_DECAY=100000):
+    """
+    Decides whether the agent should exploit or explore
+
+    :param policy_model: (callable): mapping of `obs` to q-values
+    :param obs: (np.array): current state observation
+    :param step: (int): training step count
+    :param num_actions: (int): number of actions available to the agent
+    :param EPS_START:
+    :param EPS_END:
+    :param EPS_DECAY:
+    :return: action
+    """
+
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * step / EPS_DECAY)
+    # exploit
+    if random.random() > eps_threshold:
+        action = tf.argmax(policy_model(tf.convert_to_tensor(obs)), axis=1)
+    # explore
+    else:
+        action = random.randrange(num_actions)
+
+    return action
